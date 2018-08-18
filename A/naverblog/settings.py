b71c7f4260b865fbea1b3382397b2a5c5ea97428
최 지영 <jiyoung031@gmail.com>
2018-08-18 14:47:17 +0900

    practice

--- A/naverblog/settings.py
+++ A/naverblog/settings.py
@@ -1,5 +1,5 @@
-WEB_DRIVER_PATH = 'C:\chromedriver.exe'
-XLSX_PATH = 'C:/dateGirls/m3/nanohana_final.xls'
+WEB_DRIVER_PATH = 'chromedriver'
+XLSX_PATH = 'A/crawl.csv'
 START_DATE = '20170305'
 END_DATE = '20170405'
-KEYWORD = '나노하나'
+KEYWORD = ''
